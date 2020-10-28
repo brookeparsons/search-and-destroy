e1805a431337182b4ad37d5029b1933eb97f844d
Brooke Parsons <brookep583@gmail.com>
2020-10-28 15:38:00 -0400

    passsing one spec

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,10 +1,71 @@
-'use strict';
+"use strict";
+
+// //search array for target
+// //found: return true. else: false
+// if (!array.length) {
+//   return false;
+// }
+
+// if (array.length === 1) {
+//   //given length 1, does it === target?
+//   return array[0] === target ? true : false;
+// }
+
+// let midPoint = Math.ceil(array.length / 2);
+// if (target === array[midPoint]) {
+//   //target found
+//   return true;
+// } else if (target < array[midPoint]) {
+//   //return left side of data RECURSIVELY
+//   return binarySearch(array.slice(0, midPoint));
+// } else if (target > array[midPoint]) {
+//   //return right side of data RECURSIVELY
+//   return binarySearch(array.slice(midPoint));
+//   // } else {
+//   //   // target is not a number
+//   //   return false;
+//   // }
+// } else {
+//   //no array length
+//   return false;
+// }
+
+const dummyArr = [1, 3, 4, 5, 10, 15, 20, 22, 23, 98, 1000];
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
-};
+  //search array for target
+  //found: return true. else: false
+  if (!array.length) {
+    return false;
+  }
 
+  if (array.length === 1) {
+    //given length 1, does it === target?
+    return array[0] === target ? true : false;
+  }
+
+  let midPoint = Math.ceil(array.length / 2);
+  if (target === array[midPoint]) {
+    //target found
+    return true;
+  } else if (target < array[midPoint]) {
+    //return left side of data RECURSIVELY
+    return binarySearch(array.slice(0, midPoint));
+  } else if (target > array[midPoint]) {
+    //return right side of data RECURSIVELY
+    return binarySearch(array.slice(midPoint));
+    } else {
+      // target is not a number
+      return false;
+		}
+
+  } else {
+    //no array length
+    return false;
+  }
+};
+console.log(binarySearch(dummyArr));
 /*
 	EXTRA CREDIT:
 
@@ -14,4 +75,4 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
