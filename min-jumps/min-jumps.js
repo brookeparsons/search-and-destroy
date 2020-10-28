6de7ba17313eff755c160601267c92e91f6817ce
Brooke Parsons <brookep583@gmail.com>
2020-10-28 17:59:18 -0400

    min-jumps WIP

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,26 @@
-'use strict';
+"use strict";
 
 // Complete this algo
-const minJumps = arr => {
+const minJumps = (arr, index = 0, count = 0, currentOption = 0) => {
+  //look at first num... see it's options
+  let newLength = arr.length - index;
+  debugger;
+  //base case
+  if (index >= arr.length) return count;
 
+  //only look at jump possibilities for arr[index] ..... check index + 1 and index +2
+  for (let i = index; i <= arr[index]; i++) {
+    //go over options this currentNum ha
+
+    //
+    currentOption =
+      Math.abs(currentOption - newLength) < Math.abs(arr[i] - newLength)
+        ? currentOption
+        : arr[i];
+  }
+  count++;
+  index = arr.indexOf(currentOption);
+  return minJumps(arr, index, count);
 };
 
-module.exports = minJumps
+module.exports = minJumps;
